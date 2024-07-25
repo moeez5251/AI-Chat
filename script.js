a30c04a04542af172e17d51a4bf730b9c194dbbe
Moeez <159162874+moeez5251@users.noreply.github.com>
2024-07-25 21:44:55 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ function loading() {
         }, 3500);
 }
 
+
+      
 let input = document.getElementById('text');
 let btn = document.querySelector('.btn');
 let message_container = document.querySelector('.container');
@@ -27,34 +29,83 @@ input.addEventListener('input', () => {
         }
 });
 btn.addEventListener('click', () => {
+        if (message_container.children.length > 2) {
+                if (document.querySelector('.image')) {
+
+                        document.querySelector('.image').remove();
+                        document.querySelector('.message').classList.add('message_animate');
+                        message_container.style.height = '60vh';
+                        message_container.style.marginTop = '10%';
+                }
+        }
+
         let newelement = document.createElement('div');
         newelement.classList.add('right', 'inputs');
-        newelement.innerHTML = input.value;
+        newelement.innerHTML = input.value.trim();
         message_container.appendChild(newelement);
-        const url = 'https://infinite-gpt.p.rapidapi.com/infinite-gpt';
+        let answer = document.createElement('div');
+        answer.classList.add('indicator','left','inputs');
+        message_container.appendChild(answer);
+
+        const url = 'https://cheapest-gpt-4-turbo-gpt-4-vision-chatgpt-openai-ai-api.p.rapidapi.com/v1/chat/completions';
         const options = {
                 method: 'POST',
                 headers: {
                         'x-rapidapi-key': '544727a05dmshe7e455284bae3a8p1520c7jsne7ba47ff870b',
-                        'x-rapidapi-host': 'infinite-gpt.p.rapidapi.com',
+                        'x-rapidapi-host': 'cheapest-gpt-4-turbo-gpt-4-vision-chatgpt-openai-ai-api.p.rapidapi.com',
                         'Content-Type': 'application/json'
                 },
-                body: {
-                        query: 'hi',
-                        sysMsg: 'You are a friendly Chatbot.'
-                }
+                body: 
+                JSON.stringify(
+                        {
+                                
+                                messages: [
+                                        {
+                                                role: 'user',
+                                                content: input.value
+                                        }
+                                ],
+                                model: 'gpt-4-turbo-2024-04-09',
+                                max_tokens: 100,
+                                temperature: 0.9
+                        
+                })
         };
-        async function fetchdata(){
+        async function fetching(){
 
                 try {
                         const response = await fetch(url, options);
-                const result = await response.json();
-                console.log(result);
-        } catch (error) {
-                console.error(error);
-        }
+                        const result = await response.json();
+                      return result.choices[0].message.content;
+                } catch (error) {
+                        return 'unable to connect at this moment'
                 }
-                fetchdata();
-input.value = '';
-btn.disabled = true;
+        }
+        let main; 
+function fetchDataAndStoreInGlobal() {
+  fetching() .then((result) => {
+      main = result.toString();
+      console.log(main); 
+      answer.classList.remove('indicator');
+
+      animation(answer,main)
+})
+.catch((error) => {
+        console.error('Error fetching data:', error);
 });
+}
+fetchDataAndStoreInGlobal();
+
+        input.value = '';
+        btn.disabled = true;
+});
+
+function animation(element, message) {
+        for (let i = 0; i < message.length; i++) {
+                setTimeout(() => {
+                        message_container.scrollTop=message_container.scrollHeight;
+                        element.innerHTML+= message[i];
+                }, i*10);
+         
+        }
+}
